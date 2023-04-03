1cccc76c90e198e9863f7b90f95601e275390de6
jeonghun <jeonghun@amazon.com>
2023-04-03 01:06:14 +0000

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   test.sh
    
    Changes not staged for commit:
    	modified:   start.sh

--- test.sh
+++ test.sh
@@ -1,36 +1,36 @@
 #!/bin/bash
-echo " " >> nginx.conf
-echo "     sendfile            on;" >> nginx.conf
-echo "     tcp_nopush          on;" >> nginx.conf
-echo "     tcp_nodelay         on;" >> nginx.conf
-echo "     keepalive_timeout   65;" >> nginx.conf
-echo "     types_hash_max_size 4096;" >> nginx.conf
-echo " " >> nginx.conf
-echo "     include             /etc/nginx/mime.types;" >> nginx.conf
-echo "     default_type        application/octet-stream;" >> nginx.conf
-echo " " >> nginx.conf
-echo "     include /etc/nginx/conf.d/*.conf;" >> nginx.conf
-echo " " >> nginx.conf
-echo "     server {" >> nginx.conf
-echo "         listen       80;" >> nginx.conf
-echo "         listen       [::]:80;" >> nginx.conf
-echo "         server_name  _;" >> nginx.conf
-echo " " >> nginx.conf
-echo "         location / {" >> nginx.conf
-echo "             root         /usr/share/nginx/html;" >> nginx.conf
-echo "         }" >> nginx.conf
-echo " " >> nginx.conf
-echo "         location /api {" >> nginx.conf
-echo "             proxy_pass http://{$1}:5000;" >> nginx.conf
-echo "         }" >> nginx.conf
-echo "         " >> nginx.conf
-echo "         error_page 404 /404.html;" >> nginx.conf
-echo "         location = /404.html {" >> nginx.conf
-echo "         }" >> nginx.conf
-echo " " >> nginx.conf
-echo "         error_page 500 502 503 504 /50x.html;" >> nginx.conf
-echo "         location = /50x.html {" >> nginx.conf
-echo "         }" >> nginx.conf
-echo "     }" >> nginx.conf
-echo " }" >> nginx.conf
-echo " " >> nginx.conf
+echo " " >> /home/ec2-user/web/home/ec2-user/web/nginx.conf
+echo "     sendfile            on;" >> /home/ec2-user/web/nginx.conf
+echo "     tcp_nopush          on;" >> /home/ec2-user/web/nginx.conf
+echo "     tcp_nodelay         on;" >> /home/ec2-user/web/nginx.conf
+echo "     keepalive_timeout   65;" >> /home/ec2-user/web/nginx.conf
+echo "     types_hash_max_size 4096;" >> /home/ec2-user/web/nginx.conf
+echo " " >> /home/ec2-user/web/nginx.conf
+echo "     include             /etc/nginx/mime.types;" >> /home/ec2-user/web/nginx.conf
+echo "     default_type        application/octet-stream;" >> /home/ec2-user/web/nginx.conf
+echo " " >> /home/ec2-user/web/nginx.conf
+echo "     include /etc/nginx/conf.d/*.conf;" >> /home/ec2-user/web/nginx.conf
+echo " " >> /home/ec2-user/web/nginx.conf
+echo "     server {" >> /home/ec2-user/web/nginx.conf
+echo "         listen       80;" >> /home/ec2-user/web/nginx.conf
+echo "         listen       [::]:80;" >> /home/ec2-user/web/nginx.conf
+echo "         server_name  _;" >> /home/ec2-user/web/nginx.conf
+echo " " >> /home/ec2-user/web/nginx.conf
+echo "         location / {" >> /home/ec2-user/web/nginx.conf
+echo "             root         /usr/share/nginx/html;" >> /home/ec2-user/web/nginx.conf
+echo "         }" >> /home/ec2-user/web/nginx.conf
+echo " " >> /home/ec2-user/web/nginx.conf
+echo "         location /api {" >> /home/ec2-user/web/nginx.conf
+echo "             proxy_pass http://$1:5000;" >> /home/ec2-user/web/nginx.conf
+echo "         }" >> /home/ec2-user/web/nginx.conf
+echo "         " >> /home/ec2-user/web/nginx.conf
+echo "         error_page 404 /404.html;" >> /home/ec2-user/web/nginx.conf
+echo "         location = /404.html {" >> /home/ec2-user/web/nginx.conf
+echo "         }" >> /home/ec2-user/web/nginx.conf
+echo " " >> /home/ec2-user/web/nginx.conf
+echo "         error_page 500 502 503 504 /50x.html;" >> /home/ec2-user/web/nginx.conf
+echo "         location = /50x.html {" >> /home/ec2-user/web/nginx.conf
+echo "         }" >> /home/ec2-user/web/nginx.conf
+echo "     }" >> /home/ec2-user/web/nginx.conf
+echo " }" >> /home/ec2-user/web/nginx.conf
+echo " " >> /home/ec2-user/web/nginx.conf
